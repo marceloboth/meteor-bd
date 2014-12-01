3bcd03637c63c26b3a1b3e571aadeaa1f99442e9
Marcelo J. Both <marcelo.both@gmail.com>
2014-12-01 07:20:19 -0200

    First commit

---
+++ client/lib/events/profile_form.js
@@ -0,0 +1,13 @@
+Template.profileForm.events({
+    "submit form": function(e, template) {
+        e.preventDefault();
+        var inputs = template.findAll("input");
+        Meteor.users.update(
+            { _id: this.userId },
+            { $set:
+            { "profile.name": inputs[0].value,
+                "profile.about": inputs[1].value }
+            });
+        Session.set("editProfile", false);
+    }
+});
---
+++ server/lib/services.js
@@ -0,0 +1,6 @@
+ServiceConfiguration.configurations.remove({});
+ServiceConfiguration.configurations.insert({
+    service: "facebook",
+    appId: "1571380853097527",
+    secret: "42b815f52f4cd17790f4619f509f5fd1"
+});
